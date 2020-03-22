2ca0111143810ce38e413e1ad4fb9b967339569b
whbjzzwjxq <whbjzzwjxq@gmail.com>
2020-03-22 11:24:20 +0800

    准备添加timeline 移除了不用的css

---
+++ src/components/card/standard/CardMediaSimp.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-card>
+
+    </v-card>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue'
+
+    export default Vue.extend({
+        name: "CardMediaSimp",
+        components: {},
+        data: function () {
+            return {}
+        },
+        props: {},
+        computed: {},
+        methods: {},
+        record: {
+            status: 'empty',
+            description: ''
+        }
+    })
+</script>
+
+<style scoped>
+
+</style>
